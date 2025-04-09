7eab1b40a878c994be243367868e02be052fc615
jan <scherping.jan@icloud.com>
2025-04-09 19:20:12 +0200

    move login page to /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import {  HydrateClient } from "~/trpc/server";
+import { LoginForm } from "~/components/login-form";
 
 export default async function Home() {
   return (
-    <HydrateClient>
-      <p>Hi</p>
-    </HydrateClient>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
   );
 }
