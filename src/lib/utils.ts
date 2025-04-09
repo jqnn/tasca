eaebb82aac8cd86264bf9d40a451da3d946f8c15
jan <scherping.jan@icloud.com>
2025-04-09 22:08:32 +0200

    add hashPassword function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { sha256 } from "js-sha256";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function hashPassword(password: string) {
+  return sha256(password);
 }
