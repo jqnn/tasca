59fc42b42525f0cb82590f00cf369a4ff55f5288
jan <scherping.jan@icloud.com>
2025-04-14 11:29:56 +0200

    add delete dlalog

--- src/app/dashboard/(admin)/authentication/table.tsx
+++ src/app/dashboard/(admin)/authentication/table.tsx
@@ -20,56 +20,53 @@ import { IconEdit, IconTrash } from "@tabler/icons-react";
 import { Button } from "~/components/ui/button";
 import { api } from "~/trpc/react";
 import type { AuthMethod } from "@prisma/client";
-import { undefined } from "zod";
-
-const showCreateForm = () => {
-  console.log("create");
-  return undefined;
-};
+import { Alert } from "~/components/ui/alert";
 
 const showEditForm = (id: number) => {
   console.log("edit: " + id);
-  return undefined;
 };
-
-const deleteAuthMethod = (id: number) => {
-  console.log("delete: " + id);
-  return undefined;
-};
-
-export const columns: ColumnDef<AuthMethod>[] = [
-  {
-    header: "Beschreibung",
-    cell: ({ row }) => <div>{row.original.description}</div>,
-  },
-  {
-    header: "Typ",
-    cell: ({ row }) => <div>{row.original.type}</div>,
-  },
-  {
-    header: "Aktionen",
-    cell: ({ row }) => {
-      const authMethod = row.original;
-
-      return (
-        <div className={"flex flex-row"}>
-          <IconEdit
-            className="hover:cursor-pointer"
-            onClick={() => showEditForm(authMethod.id)}
-          />
-          <IconTrash
-            className="hover:cursor-pointer"
-            onClick={() => deleteAuthMethod(authMethod.id)}
-          />
-        </div>
-      );
-    },
-  },
-];
 
 export function DataTableDemo() {
+  const [deleteId, setDeleteId] = React.useState<number | null>(null);
   const [data] = api.authMethod.findAll.useSuspenseQuery();
   const tableData = data ?? [];
+  const columns: ColumnDef<AuthMethod>[] = [
+    {
+      header: "Beschreibung",
+      cell: ({ row }) => <div>{row.original.description}</div>,
+    },
+    {
+      header: "Typ",
+      cell: ({ row }) => <div>{row.original.type}</div>,
+    },
+    {
+      header: "Aktionen",
+      cell: ({ row }) => {
+        const authMethod = row.original;
+        const disabled = authMethod.description == "local";
+        const text = disabled ? "text-muted" : "";
+
+        return (
+          <div className={"flex flex-row"}>
+            <IconEdit
+              className={"hover:cursor-pointer " + text}
+              onClick={() => {
+                if (disabled) return;
+                showEditForm(authMethod.id);
+              }}
+            />
+            <IconTrash
+              className={"hover:cursor-pointer " + text}
+              onClick={() => {
+                if (disabled) return;
+                setDeleteId(authMethod.id);
+              }}
+            />
+          </div>
+        );
+      },
+    },
+  ];
 
   const table = useReactTable({
     data: tableData,
@@ -80,7 +77,7 @@ export function DataTableDemo() {
   return (
     <div className="w-full">
       <div className="flex items-center pb-4">
-        <Button variant="outline" className="mr-auto" onClick={() => showCreateForm()}>
+        <Button variant="outline" className="mr-auto">
           Hinzufügen
         </Button>
       </div>
@@ -132,6 +129,17 @@ export function DataTableDemo() {
           </TableBody>
         </Table>
       </div>
+
+      <Alert
+        open={deleteId !== null}
+        setOpen={() => {
+          setDeleteId(deleteId);
+        }}
+        texts={{
+          confirmText: "Löschen",
+        }}
+        onConfirm={() => console.log("test")}
+      />
     </div>
   );
 }
