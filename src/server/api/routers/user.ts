819a47668ee853750af152dd32a947921a3fe304
jan <scherping.jan@icloud.com>
2025-04-15 14:28:58 +0200

    add findAll method

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,6 +2,9 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
+  findAll: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.user.findMany();
+  }),
   isAdmin: publicProcedure
     .input(z.object({ id: z.number() }))
     .query(async ({ ctx, input }) => {
