f4c310c31711ed72099771924de07941e01a39b7
jan <scherping.jan@icloud.com>
2025-04-15 14:29:06 +0200

    better structure

--- src/app/dashboard/(admin)/authentication/page.tsx
+++ src/app/dashboard/(admin)/authentication/page.tsx
@@ -18,7 +18,11 @@ export default async function AuthenticationMethods() {
   return (
     <>
       <SiteHeader title={"Authentifizierungsmethoden"} />
-      <AuthenticationMethodsTable />
+      <main className={"flex shrink-0 items-center gap-2 transition-[width,height] ease-linear"}>
+        <div className={"flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6"}>
+          <AuthenticationMethodsTable/>
+        </div>
+      </main>
     </>
   );
 }
