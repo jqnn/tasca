2af843637d1dde713078277dfa90002dfbc19e4f
jan <scherping.jan@icloud.com>
2025-04-17 14:31:11 +0200

    start with template page

--- src/app/dashboard/(admin)/templates/[id]/page.tsx
+++ src/app/dashboard/(admin)/templates/[id]/page.tsx
@@ -1,3 +1,47 @@
-export default function TemplatePage() {
-  return <p>TODO</p>
+"use client";
+
+import { redirect } from "next/navigation";
+import React from "react";
+import { useSession } from "next-auth/react";
+import { api } from "~/trpc/react";
+import { SiteHeader } from "~/components/ui/site-header";
+
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default function TemplatePage({ params }: PageProps) {
+  const actualParams = React.use(params);
+  const { data: session } = useSession();
+  if (!session) {
+    redirect("/");
+  }
+
+  const [role] = api.user.getRole.useSuspenseQuery({
+    id: Number(session.user?.id),
+  });
+
+  if (role !== "ADMINISTRATOR") {
+    redirect("/dashboard");
+  }
+
+  const { data: project, status } = api.template.find.useQuery({
+    id: Number(Number(actualParams.id)),
+  });
+
+  if (status !== "success") {
+    return <p>Lädt...</p>;
+  }
+
+  if (!project) {
+    return <p>Kein Projekt gefunden.</p>;
+  }
+
+  return (
+    <>
+      <SiteHeader title={"Projekt - " + project.name} />
+    </>
+  );
 }
