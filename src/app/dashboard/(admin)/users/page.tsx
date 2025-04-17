4aad99ffe0fdf72b24da903dd9e14e8c035ed330
jan <scherping.jan@icloud.com>
2025-04-17 15:18:55 +0200

    remove old permission checks

--- src/app/dashboard/(admin)/users/page.tsx
+++ src/app/dashboard/(admin)/users/page.tsx
@@ -1,17 +1,11 @@
 import { SiteHeader, SiteTitle } from "~/components/ui/site-header";
 import { auth } from "~/server/auth";
 import { redirect } from "next/navigation";
-import { api } from "~/trpc/server";
 import UsersTable from "~/app/dashboard/(admin)/users/table";
 
 export default async function Users() {
   const session = await auth();
   if (!session) {
-    redirect("/");
-  }
-
-  const role = await api.user.getRole({ id: Number(session.user.id) });
-  if (role != "ADMINISTRATOR") {
     redirect("/");
   }
 
