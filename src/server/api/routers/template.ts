0ac6a3f93e6a6bee946379990706b09f90b23441
jan <scherping.jan@icloud.com>
2025-04-17 14:07:26 +0200

    add exists and create mutation

--- src/server/api/routers/template.ts
+++ src/server/api/routers/template.ts
@@ -1,7 +1,36 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
 
 export const templateRouter = createTRPCRouter({
-    findAll: publicProcedure.query(async ({ ctx }) => {
-        return ctx.db.template.findMany();
+  findAll: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.template.findMany();
+  }),
+
+  exists: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.template
+        .findFirst({ where: { name: input.name } })
+        .then((template) => {
+          return template != null;
+        });
+    }),
+
+  create: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string().nullable(),
+        userId: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.template.create({
+        data: {
+          name: input.name,
+          description: input.description ?? null,
+          createdById: Number(input.userId),
+        },
+      });
     }),
 });
