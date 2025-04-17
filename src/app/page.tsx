9232fd0bdb5c98ecc0d196802d59abbb7783ca2e
jan <scherping.jan@icloud.com>
2025-04-17 12:10:34 +0200

    remove old seeding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import { LoginForm } from "~/components/login-form";
 import {auth} from "~/server/auth";
 import {redirect} from "next/navigation";
-import {api} from "~/trpc/server";
 
 export default async function LoginPage() {
-   void api.seed.createDefault.prefetch()
-
   const session = await auth();
   if (session) {
     redirect("/dashboard");
