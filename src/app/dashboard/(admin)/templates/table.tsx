29bf170718d8cd75c648b067c1ec22046d2102be
jan <scherping.jan@icloud.com>
2025-04-18 12:42:05 +0200

    implement DeleteTemplateDialog component

--- src/app/dashboard/(admin)/templates/table.tsx
+++ src/app/dashboard/(admin)/templates/table.tsx
@@ -8,11 +8,14 @@ import type { Template } from "@prisma/client";
 import { DataTable } from "~/components/ui/data-table";
 import CreateTemplateDialog from "~/app/dashboard/(admin)/templates/(dialogs)/create-template";
 import Link from "next/link";
+import { IconTrash } from "@tabler/icons-react";
+import { DeleteTemplateDialog } from "~/app/dashboard/(admin)/templates/(dialogs)/delete-template";
 
 export default function TemplateTable() {
   const { data, isLoading } = api.template.findAll.useQuery();
   const [tableData, setTableData] = React.useState<Template[]>([]);
   const [createOpen, setCreateOpen] = React.useState<boolean>(false);
+  const [deleteId, setDeleteId] = React.useState<number | null>(null);
 
   React.useEffect(() => {
     if (!isLoading) {
@@ -22,7 +25,14 @@ export default function TemplateTable() {
   const columns: ColumnDef<Template>[] = [
     {
       header: "Name",
-      cell: ({ row }) => <Link className={"font-bold"} href={`/dashboard/templates/${row.original.id}`}>{row.original.name}</Link>,
+      cell: ({ row }) => (
+        <Link
+          className={"font-bold"}
+          href={`/dashboard/templates/${row.original.id}`}
+        >
+          {row.original.name}
+        </Link>
+      ),
     },
     {
       header: "Beschreibung",
@@ -32,15 +42,33 @@ export default function TemplateTable() {
       header: "Ersteller",
       cell: ({ row }) => {
         const template = row.original;
-        const { data: user, isLoading } = api.user.find.useQuery({ id: template.createdById });
+        const { data: user, isLoading } = api.user.find.useQuery({
+          id: template.createdById,
+        });
 
-        if(isLoading || !(user)) return <div>Unbekannt</div>;
+        if (isLoading || !user) return <div>Unbekannt</div>;
         return <div>{user.displayName ?? user.userName}</div>;
       },
     },
     {
       header: "Erstellt am",
       cell: ({ row }) => <div>{row.original.createdAt.toLocaleString()}</div>,
+    },
+    {
+      accessorKey: "actions",
+      header: () => <div className="text-center">Aktionen</div>,
+      cell: ({ row }) => {
+        return (
+          <div className={"flex flex-row justify-center text-center"}>
+            <IconTrash
+              className={"text-center hover:cursor-pointer"}
+              onClick={() => {
+                setDeleteId(row.original.id);
+              }}
+            />
+          </div>
+        );
+      },
     },
   ];
 
@@ -56,6 +84,18 @@ export default function TemplateTable() {
         setOpen={setCreateOpen}
         onCreate={(data) => {
           setTableData([...tableData, data]);
+        }}
+      />
+
+      <DeleteTemplateDialog
+        open={deleteId !== null}
+        setOpen={(value) => {
+          if (value) return;
+          setDeleteId(null);
+        }}
+        templateId={deleteId}
+        onDelete={() => {
+          setTableData(tableData.filter((item) => item.id !== deleteId));
         }}
       />
     </DataTable>
