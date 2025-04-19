b1c8f65faaf5b2a02b4d5d3fc2cd1dbb665c818a
jan <scherping.jan@icloud.com>
2025-04-19 13:56:21 +0200

    create TableActions helper

---
+++ src/components/table/table-actions.tsx
@@ -0,0 +1,52 @@
+import type { Row } from "@tanstack/react-table";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
+import * as React from "react";
+
+interface RowData {
+  id: number;
+}
+
+export default function TableActions<TData extends RowData>(
+  onEditClick?: ((value: number) => void) | null,
+  onDeleteClick?: ((value: number) => void) | null,
+  disabled?: ((value: TData) => boolean) | null,
+) {
+  return {
+    accessorKey: "actions",
+    header: () => <div className={"text-center"}>Aktionen</div>,
+    cell: ({ row }: { row: Row<TData> }) => {
+      const id = row.original.id ?? 0;
+      const value = row.original;
+      if (value === undefined || value === null) {
+        return null;
+      }
+      const isDisabled = disabled ? disabled(value) : false;
+      const disabledStyle = disabled ? "text-muted" : "";
+
+      return (
+        <div className={"flex flex-row justify-center gap-2"}>
+          {onEditClick && (
+            <IconEdit
+              className={`hover:cursor-pointer ${disabledStyle}`}
+              onClick={() => {
+                if (isDisabled) return;
+                if (!onEditClick) return;
+                onEditClick(id);
+              }}
+            />
+          )}
+          {onDeleteClick && (
+            <IconTrash
+              className={`hover:cursor-pointer ${disabledStyle}`}
+              onClick={() => {
+                if (isDisabled) return;
+                if (!onDeleteClick) return;
+                onDeleteClick(id);
+              }}
+            />
+          )}
+        </div>
+      );
+    },
+  };
+}
