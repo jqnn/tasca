38a358f1888a26202073d7ec3878d131b55902c7
jan <scherping.jan@icloud.com>
2025-04-19 16:05:50 +0200

    add page for specific task

---
+++ src/app/dashboard/tasks/[id]/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import React from "react";
+import { useSession } from "next-auth/react";
+import {
+  SiteHeader,
+  SiteHeaderSkeleton,
+  SiteTitle,
+} from "~/components/ui/site-header";
+import { api } from "~/trpc/react";
+
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default function TaskPage({ params }: PageProps) {
+  const actualParams = React.use(params);
+  const { data: session } = useSession();
+  if (!session) {
+    redirect("/");
+  }
+
+  const { data: instance, status } = api.instance.find.useQuery({
+    id: Number(actualParams.id),
+  });
+
+  if (status !== "success") {
+    return (
+      <>
+        <SiteHeaderSkeleton />
+      </>
+    );
+  }
+
+  if (!instance) {
+    return <p>Keine Aufgabe gefunden.</p>;
+  }
+
+  return (
+    <>
+      <SiteHeader>
+        <SiteTitle title={"Aufgabe - " + instance.template.name} />
+      </SiteHeader>
+
+      <main className="flex shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+        <div className={"flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6"}>
+          <p>TODO</p>
+        </div>
+      </main>
+    </>
+  );
+}
