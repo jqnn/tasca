f4a47c9d7f188d22a35a962a1caeb1f57f3ca9a9
jan <scherping.jan@icloud.com>
2025-04-19 16:19:33 +0200

    move useState to client side component

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -1,7 +1,6 @@
 import { SiteHeader, SiteTitle } from "~/components/ui/site-header";
 import * as React from "react";
 import { TaskList } from "~/app/dashboard/tasks/task-list";
-import { Button } from "~/components/ui/button";
 
 export default function Tasks() {
   return (
@@ -13,12 +12,6 @@ export default function Tasks() {
       <main className="flex shrink-0 items-center gap-2 transition-[width,height] ease-linear">
         <div className={"flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6"}>
           <div className={"w-full"}>
-            <div className="flex items-center pb-4">
-              <Button variant="outline" className="mr-auto">
-                Erstellen
-              </Button>
-            </div>
-
             <TaskList />
           </div>
         </div>
--- src/app/dashboard/tasks/task-list.tsx
+++ src/app/dashboard/tasks/task-list.tsx
@@ -12,6 +12,9 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { api } from "~/trpc/react";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { Switch } from "~/components/ui/switch";
+import { Label } from "~/components/ui/label";
 
 export function TaskList() {
   const { data: session } = useSession();
@@ -19,39 +22,51 @@ export function TaskList() {
     redirect("/");
   }
 
-  const { data: tasks, status } = api.instance.findAll.useQuery();
-
-  if (status !== "success") {
-    return (
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <Card key={index}>
-            <CardHeader>
-              <Skeleton className="h-4 max-w-1/3" />
-              <Skeleton className="mt-1.5 h-4 max-w-2/3" />
-            </CardHeader>
-          </Card>
-        ))}
-      </div>
-    );
-  }
-
-  if (!tasks || tasks.length <= 0) {
-    return <p>Keine Aufgaben.</p>;
-  }
+  const [showComplete, setShowComplete] = React.useState(false);
+  const { data: tasks, status } = api.instance.findAll.useQuery({
+    completed: showComplete,
+  });
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {tasks.map((task) => (
-        <Link key={task.id} href={`/dashboard/tasks/${task.id}`}>
-          <Card key={task.id}>
-            <CardHeader>
-              <CardTitle>{task.template.name}</CardTitle>
-              <CardDescription>{task.status}</CardDescription>
-            </CardHeader>
-          </Card>
-        </Link>
-      ))}
+    <div>
+      <div className="flex items-center pb-4">
+        <Button variant="outline" className="mr-auto">
+          Erstellen
+        </Button>
+
+        <div className="flex items-center space-x-2">
+          <Switch checked={showComplete} onCheckedChange={setShowComplete} id="completed" />
+          <Label htmlFor="completed">Fertige anzeigen</Label>
+        </div>
+      </div>
+
+      {status !== "success" && (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Card key={index}>
+              <CardHeader>
+                <Skeleton className="h-4 max-w-1/3" />
+                <Skeleton className="mt-1.5 h-4 max-w-2/3" />
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
+      )}
+
+      {status === "success" && (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {tasks.map((task) => (
+            <Link key={task.id} href={`/dashboard/tasks/${task.id}`}>
+              <Card key={task.id}>
+                <CardHeader>
+                  <CardTitle>{task.template.name}</CardTitle>
+                  <CardDescription>{task.status}</CardDescription>
+                </CardHeader>
+              </Card>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
