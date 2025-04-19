864a4f56af3182d40ca1b7826524384e196b2d08
jan <scherping.jan@icloud.com>
2025-04-19 16:04:33 +0200

    add find specifc route

--- src/server/api/routers/instances.ts
+++ src/server/api/routers/instances.ts
@@ -1,11 +1,25 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const instanceRouter = createTRPCRouter({
   findAll: publicProcedure.query(async ({ ctx }) => {
     return ctx.db.templateInstance.findMany({
       include: {
-        template: true
-      }
+        template: true,
+      },
     });
   }),
+
+  find: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.templateInstance.findUnique({
+        where: { id: input.id },
+        include: {
+          template: true,
+          FieldInstance: true,
+          TaskInstance: true,
+        },
+      });
+    }),
 });
