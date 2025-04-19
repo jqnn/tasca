a4dabf5e167ace04b4c1374af9e29ff3070ae587
jan <scherping.jan@icloud.com>
2025-04-19 15:24:44 +0200

    add CreateTemplateFieldDialog and create table

---
+++ src/app/dashboard/(admin)/templates/[id]/fields-table.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import * as React from "react";
+import { useEffect } from "react";
+import { type ColumnDef } from "@tanstack/react-table";
+
+import type { TemplateField } from "@prisma/client";
+import { SortableDataTable } from "~/components/table/sortable-table";
+import { api } from "~/trpc/react";
+import { centeredColumn } from "~/components/table/table";
+import TableActions from "~/components/table/table-actions";
+import { DeleteDialog } from "~/components/dialogs/delete-dialog";
+import CreateTemplateFieldDialog from "~/app/dashboard/(admin)/templates/[id]/(dialogs)/create-template-field";
+import { showToast } from "~/lib/utils";
+
+export default function TemplateFieldsTable({
+  templateId,
+  fields,
+}: {
+  templateId: number;
+  fields: TemplateField[];
+}) {
+  const [createOpen, setCreateOpen] = React.useState<boolean>(false);
+  const [deleteId, setDeleteId] = React.useState<number | null>(null);
+  const [tableData, setTableData] = React.useState<TemplateField[]>([]);
+
+  const deleteMutation = api.templateField.delete.useMutation();
+  const updateOrderMutation = api.templateField.updateOrder.useMutation({
+    onMutate: () => {
+      showToast("Lädt", "Die Reihenfolge wird aktualisiert...");
+    },
+    onSuccess: () => {
+      showToast("Erledigt", "Die Reihenfolge wurde aktualisiert.");
+    },
+    onError: () => {
+      showToast(
+        "Unerwarteter Fehler",
+        "Möglicherweise hat diese Authentifiezerungsmethode noch Benutzer.",
+      );
+    },
+  });
+
+  useEffect(() => {
+    setTableData(fields.sort((a, b) => a.order - b.order));
+  }, [fields]);
+
+  const columns: ColumnDef<TemplateField>[] = [
+    centeredColumn("label", "Bezeichnung"),
+    centeredColumn("placeHolder", "Platzhalter"),
+    centeredColumn("fieldType", "Typ"),
+    TableActions(null, (id) => setDeleteId(id)),
+  ];
+
+  return (
+    <div className="mt-8 w-full">
+      <SortableDataTable
+        data={tableData}
+        columns={columns}
+        loading={false}
+        onButtonClick={() => setCreateOpen(true)}
+        onSaveButtonClick={(data) => {
+          updateOrderMutation.mutate({
+            templateId: templateId,
+            fields: data,
+          });
+        }}
+      >
+
+        <CreateTemplateFieldDialog
+          templateId={templateId}
+          order={tableData.length + 1}
+          open={createOpen}
+          setOpen={setCreateOpen}
+          onCreate={(data) => {
+            setTableData([...tableData, data]);
+          }}
+        />
+
+        <DeleteDialog
+          open={deleteId !== null}
+          setOpen={(value) => {
+            if (value) return;
+            setDeleteId(null);
+          }}
+          mutation={deleteMutation}
+          data={{ id: deleteId ?? 0 }}
+          onDelete={() => {
+            setTableData(tableData.filter((item) => item.id !== deleteId));
+          }}
+        />
+      </SortableDataTable>
+    </div>
+  );
+}
