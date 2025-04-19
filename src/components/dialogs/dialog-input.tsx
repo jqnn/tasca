2fd5e1480fb57c9b62f78f762abdaba4e8855e13
jan <scherping.jan@icloud.com>
2025-04-19 15:14:17 +0200

    create DialogInput helper component

---
+++ src/components/dialogs/dialog-input.tsx
@@ -0,0 +1,42 @@
+import { Label } from "~/components/ui/label";
+import { Input } from "~/components/ui/input";
+import * as React from "react";
+import type { HTMLInputTypeAttribute } from "react";
+
+export default function DialogInput({
+  id,
+  label,
+  placeHolder,
+  required = false,
+  setValue,
+  min,
+  max,
+  type = "text"
+}: {
+  id: string;
+  label: string;
+  placeHolder: string;
+  required: boolean;
+  setValue: (open: string) => void;
+  min: number | string;
+  max: number | string;
+  type: HTMLInputTypeAttribute;
+}) {
+  return (
+    <div className="grid grid-cols-4 items-center gap-4">
+      <Label htmlFor={id} className="text-right">
+        {label}
+      </Label>
+      <Input
+        id={id}
+        className="col-span-3"
+        placeholder={placeHolder ?? ""}
+        required={required}
+        onChange={(e) => setValue(e.target.value)}
+        min={min}
+        max={max}
+        type={type}
+      />
+    </div>
+  );
+}
