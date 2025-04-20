8bdcb4509bcfe32e9f793ad6fdee21092ba43330
jan <scherping.jan@icloud.com>
2025-04-20 13:53:07 +0200

    create FieldInput component

---
+++ src/components/instance/field-input.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Label } from "~/components/ui/label";
+import { Input } from "~/components/ui/input";
+import * as React from "react";
+import type { InstanceField, TemplateField } from "@prisma/client";
+import { api } from "~/trpc/react";
+import { showErrorToast } from "~/lib/utils";
+
+export default function FieldInput({
+  instance,
+  field,
+}: {
+  instance: InstanceField;
+  field: TemplateField;
+}) {
+  const handleBlur = () => {
+    updateMutation.mutate({
+      id: instance.id,
+      value: value,
+    });
+  };
+
+  const updateMutation = api.instance.updateValue.useMutation({
+    onError: () => {
+      showErrorToast();
+    },
+  });
+
+  const [value, setValue] = React.useState<string>("");
+
+  return (
+    <div className="grid grid-cols-8 gap-4">
+      <Label className={"ml-auto mr-auto"} htmlFor={String(instance.id)}>{field.label}</Label>
+      <Input
+        id={String(instance.id)}
+        value={instance.value}
+        className={"col-span-7"}
+        placeholder={field.placeHolder ?? ""}
+        type={field.fieldType}
+        onChange={(e) => setValue(e.target.value)}
+        onBlur={handleBlur}
+      />
+    </div>
+  );
+}
