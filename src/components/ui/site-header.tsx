439ee316d436408985cfdae0ebec9bdfd66c00e2
jan <scherping.jan@icloud.com>
2025-04-22 17:50:14 +0200

    add collapsible sidebar

--- src/components/ui/site-header.tsx
+++ src/components/ui/site-header.tsx
@@ -1,11 +1,19 @@
 import type { ReactNode } from "react";
 import * as React from "react";
 import { cn } from "~/lib/utils";
+import { SidebarTrigger } from "~/components/ui/sidebar";
+import { Separator } from "~/components/ui/separator";
 
 export function SiteHeader({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <header className="flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
-      <div className="flex h-full w-full items-center gap-1 px-4 align-middle lg:gap-2 lg:px-6">
+    <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
+      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+          orientation="vertical"
+          className="mx-2 data-[orientation=vertical]:h-4"
+        />
+
         {children}
       </div>
     </header>
@@ -13,7 +21,7 @@ export function SiteHeader({ children }: Readonly<{ children: ReactNode }>) {
 }
 
 export function SiteTitle({ title }: { title: string }) {
-  return <h1 className={cn("leading-none font-semibold")}>{title}</h1>;
+  return <h1 className="text-base font-medium">{title}</h1>;
 }
 
 export function SiteDescription({ description }: { description: string }) {
