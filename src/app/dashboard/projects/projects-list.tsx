dfb642373bf55774717671e1a3518cbbec5a064b
jan <scherping.jan@icloud.com>
2025-04-23 19:43:53 +0200

    add custom title and description when project is personal

--- src/app/dashboard/projects/projects-list.tsx
+++ src/app/dashboard/projects/projects-list.tsx
@@ -50,27 +50,43 @@ export function ProjectList() {
 
       {projects && (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {projects.map((project) => (
-            <Link key={project.id} href={`/dashboard/projects/${project.id}`}>
-              <Card>
-                <CardHeader>
-                  <CardTitle>{project.name}</CardTitle>
-                  <CardDescription>
-                    <p>
-                      Ersteller -{" "}
-                      {project.createdBy.displayName ??
-                        project.createdBy.userName}
-                    </p>
-                    <p>Mitglieder - {project.ProjectMember.length}</p>
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-            </Link>
-          ))}
+          {projects.map((project) => {
+            const title = project.personal
+              ? (project.createdBy.displayName ?? project.createdBy.userName)
+              : project.name;
+
+            const description = project.personal ? (
+              <>
+                <p>persönliches Projekt</p>
+                <p>Aufgaben - 0</p>
+              </>
+            ) : (
+              <>
+                {project.description && (
+                  <p>Beschreibung - {project.description}</p>
+                )}
+                <p>
+                  Ersteller -{" "}
+                  {project.createdBy.displayName ?? project.createdBy.userName}
+                </p>
+              </>
+            );
+
+            return (
+              <Link key={project.id} href={`/dashboard/projects/${project.id}`}>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{title}</CardTitle>
+                    <CardDescription>{description}</CardDescription>
+                  </CardHeader>
+                </Card>
+              </Link>
+            );
+          })}
         </div>
       )}
 
-      {(role !== "USER" && showCreating) && (
+      {role !== "USER" && showCreating && (
         <CreateProjectDialog
           open={showCreating}
           setOpen={setShowCreating}
