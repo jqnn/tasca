4a5aa8b4aaaa5a50c555e609a924f4c1a6fb9454
jan <scherping.jan@icloud.com>
2025-04-23 19:22:29 +0200

    add create button

--- src/app/dashboard/projects/projects-list.tsx
+++ src/app/dashboard/projects/projects-list.tsx
@@ -12,8 +12,11 @@ import { useRouter } from "next/navigation";
 import { api } from "~/trpc/react";
 import Link from "next/link";
 import Spinner from "~/components/ui/spinner";
+import { Button } from "~/components/ui/button";
 
 export function ProjectList() {
+  const [showCreating, setShowCreating] = React.useState(false);
+
   const router = useRouter();
   const { data: session } = useSession();
   if (!session) {
@@ -29,6 +32,16 @@ export function ProjectList() {
 
   return (
     <div>
+      <div className="flex items-center pb-4">
+        <Button
+          variant="outline"
+          className="mr-auto"
+          onClick={() => setShowCreating(true)}
+        >
+          Erstellen
+        </Button>
+      </div>
+
       {projects && (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {projects.map((project) => (
@@ -39,7 +52,8 @@ export function ProjectList() {
                   <CardDescription>
                     <p>
                       Ersteller -{" "}
-                      {project.createdBy.displayName ?? project.createdBy.userName}
+                      {project.createdBy.displayName ??
+                        project.createdBy.userName}
                     </p>
                     <p>Mitgleider - {project.ProjectMember.length}</p>
                   </CardDescription>
