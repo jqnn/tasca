77c0cafb7be5b322a0f0b1c8a9174641702e4b8d
jan <scherping.jan@icloud.com>
2025-04-23 20:03:51 +0200

    add project page
    WIP

---
+++ src/app/dashboard/projects/[id]/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useSession } from "next-auth/react";
+import {
+  SiteDescription,
+  SiteHeader,
+  SiteTitle,
+} from "~/components/ui/site-header";
+import { api } from "~/trpc/react";
+import Spinner from "~/components/ui/spinner";
+
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default function ProjectPage({ params }: PageProps) {
+  const router = useRouter();
+  const actualParams = React.use(params);
+  const { data: session } = useSession();
+  if (!session) {
+    router.push("/");
+    return;
+  }
+
+  const { data: project, status } = api.project.find.useQuery({
+    id: Number(actualParams.id),
+  });
+
+  if (status !== "success") {
+    return <Spinner />;
+  }
+
+  if (!project) {
+    return <p>Kein Projekt gefunden.</p>;
+  }
+
+  const title = project.personal
+    ? (project.createdBy.displayName ?? project.createdBy.userName)
+    : project.name;
+
+  const description = project.personal
+    ? "persönliches Projekt"
+    : project.description;
+
+  return (
+    <>
+      <SiteHeader>
+        <SiteTitle title={`Projekt - ${title}`} />
+        {description && (
+          <SiteDescription description={description} />
+        )}
+      </SiteHeader>
+
+      <main className="flex shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+        <div className="flex w-full flex-col items-center gap-1 px-4 lg:gap-2 lg:px-6">
+          <p>TODO</p>
+        </div>
+      </main>
+    </>
+  );
+}
