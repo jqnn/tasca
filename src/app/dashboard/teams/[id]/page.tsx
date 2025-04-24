043a30b4431feca1bd296727db17137232ccfa7c
jan <scherping.jan@icloud.com>
2025-04-24 19:15:53 +0200

    Update page.tsx

--- src/app/dashboard/teams/[id]/page.tsx
+++ src/app/dashboard/teams/[id]/page.tsx
@@ -1,65 +1,16 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import React from "react";
-import { useSession } from "next-auth/react";
-import {
-  SiteDescription,
-  SiteHeader,
-  SiteTitle,
-} from "~/components/ui/site-header";
-import { api } from "~/trpc/react";
-import Spinner from "~/components/ui/spinner";
+import { useTeam } from "~/context/TeamProvider";
+import { notFound } from "next/navigation";
 
-interface PageProps {
-  params: Promise<{
-    id: string;
-  }>;
-}
-
-export default function ProjectPage({ params }: PageProps) {
-  const router = useRouter();
-  const actualParams = React.use(params);
-  const { data: session } = useSession();
-  if (!session) {
-    router.push("/");
-    return;
+export default function ProjectPage() {
+  const team = useTeam()
+  if(!(team)) {
+    return notFound()
   }
-
-  const { data: project, status } = api.team.find.useQuery({
-    id: Number(actualParams.id),
-  });
-
-  if (status !== "success") {
-    return <Spinner />;
-  }
-
-  if (!project) {
-    return <p>Kein Projekt gefunden.</p>;
-  }
-
-  const title = project.personal
-    ? (project.createdBy.displayName ?? project.createdBy.userName)
-    : project.name;
-
-  const description = project.personal
-    ? "persönliches Projekt"
-    : project.description;
 
   return (
-    <>
-      <SiteHeader>
-        <SiteTitle title={`Team - ${title}`} />
-        {description && (
-          <SiteDescription description={description} />
-        )}
-      </SiteHeader>
-
-      <main className="flex shrink-0 items-center gap-2 transition-[width,height] ease-linear">
-        <div className="flex w-full flex-col items-center gap-1 px-4 lg:gap-2 lg:px-6">
-          <p>TODO</p>
-        </div>
-      </main>
-    </>
+    <p>{team.name}</p>
   );
 }
