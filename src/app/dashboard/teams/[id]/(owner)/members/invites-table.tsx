6d40649f114bd0a84ddfff159f204c721df83812
jan <scherping.jan@icloud.com>
2025-04-24 20:44:08 +0200

    create TeamInvitesTable component

---
+++ src/app/dashboard/teams/[id]/(owner)/members/invites-table.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import * as React from "react";
+import { type ColumnDef } from "@tanstack/react-table";
+
+import { api } from "~/trpc/react";
+import type { TeamInvite } from "@prisma/client";
+import { DataTable } from "~/components/table/data-table";
+import Spinner from "~/components/ui/spinner";
+import { useTeam } from "~/context/TeamProvider";
+import { notFound } from "next/navigation";
+import { centeredColumn } from "~/components/table/table";
+import TableActions from "~/components/table/table-actions";
+import { DeleteDialog } from "~/components/dialogs/delete-dialog";
+
+export default function TeamInvitesTable() {
+  const team = useTeam();
+  const { data, status } = api.teamInvites.findTeams.useQuery({
+    id: team.team.id,
+  });
+  const [tableData, setTableData] = React.useState<TeamInvite[]>([]);
+  const [deleteId, setDeleteId] = React.useState<number | null>(null);
+
+  const deleteMutation = api.teamInvites.delete.useMutation();
+
+  React.useEffect(() => {
+    setTableData(data ?? []);
+  }, [data]);
+
+  if (!team) {
+    return notFound();
+  }
+
+  if (status !== "success") {
+    return <Spinner />;
+  }
+
+  const columns: ColumnDef<TeamInvite>[] = [
+    centeredColumn("userId", "Benutzer", (value) => {
+      const { data: user, isLoading } = api.user.find.useQuery({ id: value });
+      if (isLoading || !user) return "Unbekannt";
+      return user.displayName ?? user.userName;
+    }),
+    centeredColumn("sentAt", "Gesendet am", (value) => value.toLocaleString()),
+    TableActions(null, (value) => setDeleteId(value)),
+  ];
+
+  return (
+    <DataTable data={tableData} columns={columns}>
+      {deleteId && (
+        <DeleteDialog
+          open={true}
+          setOpen={(value) => {
+            if (value) return;
+            setDeleteId(null);
+          }}
+          data={{ id: deleteId ?? 0 }}
+          mutation={deleteMutation}
+          onDelete={() => {
+            setTableData(tableData.filter((item) => item.userId !== deleteId));
+          }}
+        />
+      )}
+    </DataTable>
+  );
+}
