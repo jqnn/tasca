52072dd2d5d08914467585aaeac0de41f348904a
jan <scherping.jan@icloud.com>
2025-04-24 21:17:48 +0200

    create TeamProcessesTable

---
+++ src/app/dashboard/teams/[id]/(users)/processes/processes-table.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import * as React from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { api } from "~/trpc/react";
+import { Button } from "~/components/ui/button";
+import { Switch } from "~/components/ui/switch";
+import { Label } from "~/components/ui/label";
+import CreateTaskByTemplateDialog from "~/app/dashboard/teams/[id]/(users)/processes/(dialogs)/create-task";
+import Spinner from "~/components/ui/spinner";
+import { TaskCardComponent } from "~/components/cards/task-card";
+import { useTeam } from "~/context/TeamProvider";
+
+export function TeamProcessesTable() {
+  const team = useTeam();
+  const [showModal, setShowModal] = React.useState(false);
+  const [showComplete, setShowComplete] = React.useState(false);
+
+  const router = useRouter();
+  const { data: session } = useSession();
+  if (!session) {
+    router.push("/");
+    return;
+  }
+
+  const { data: tasks, status } = api.instance.findAll.useQuery({
+    teamId: team.team.id,
+    completed: showComplete,
+  });
+
+  if (status !== "success") {
+    return <Spinner />;
+  }
+
+  return (
+    <div className={"w-full"}>
+      <div className="flex items-center pb-4">
+        <Button
+          variant="outline"
+          className="mr-auto"
+          onClick={() => setShowModal(true)}
+        >
+          Erstellen
+        </Button>
+
+        <div className="flex items-center space-x-2">
+          <Switch
+            checked={showComplete}
+            onCheckedChange={setShowComplete}
+            id="completed"
+          />
+          <Label htmlFor="completed">Fertige anzeigen</Label>
+        </div>
+      </div>
+
+      {tasks && (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {tasks.map((task) => (
+            <TaskCardComponent key={task.id} task={task} />
+          ))}
+        </div>
+      )}
+
+      {showModal && (
+        <CreateTaskByTemplateDialog
+          open={showModal}
+          setOpen={setShowModal}
+          onCreate={(instance) => {
+            router.push(`/dashboard/teams/${team.team.id}/processes/${instance.id}`);
+          }}
+        />
+      )}
+    </div>
+  );
+}
