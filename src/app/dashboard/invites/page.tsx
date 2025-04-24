61a2f0fdc87136223a95f68787b4eaa51b09f888
jan <scherping.jan@icloud.com>
2025-04-24 21:30:50 +0200

    create invites page

--- src/app/dashboard/invites/page.tsx
+++ src/app/dashboard/invites/page.tsx
@@ -1,18 +1,18 @@
 import { SiteHeader, SiteTitle } from "~/components/ui/site-header";
 import * as React from "react";
-import { TaskList } from "~/app/dashboard/tasks/task-list";
+import InviteList from "~/app/dashboard/invites/invite-list";
 
 export default function Tasks() {
   return (
     <>
       <SiteHeader>
-        <SiteTitle title={"Aufgaben"} />
+        <SiteTitle title={"Einladungen"} />
       </SiteHeader>
 
       <main className="flex shrink-0 items-center gap-2 transition-[width,height] ease-linear">
         <div className={"flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6"}>
           <div className={"w-full"}>
-            <TaskList />
+            <InviteList />
           </div>
         </div>
       </main>
