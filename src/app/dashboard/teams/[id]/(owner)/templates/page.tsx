0bff85d489ebe0aaecb239add2b298fa6ef49860
jan <scherping.jan@icloud.com>
2025-04-24 20:56:42 +0200

    rename file

--- src/app/dashboard/teams/[id]/(owner)/templates/page.tsx
+++ src/app/dashboard/teams/[id]/(owner)/templates/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useTeam } from "~/context/TeamProvider";
 import { notFound } from "next/navigation";
-import TeamTemplatesTable from "~/app/dashboard/teams/[id]/(owner)/templates/table";
+import TeamTemplatesTable from "~/app/dashboard/teams/[id]/(owner)/templates/templates-table";
 
 export default function TeamTemplatesPage() {
   const team = useTeam()
