4377c55ccbb216fa6f851e3329abffb546941376
jan <scherping.jan@icloud.com>
2025-04-24 20:44:16 +0200

    add tabs and implement TeamInvitesTable component

--- src/app/dashboard/teams/[id]/(owner)/members/page.tsx
+++ src/app/dashboard/teams/[id]/(owner)/members/page.tsx
@@ -4,9 +4,12 @@ import React from "react";
 import { useTeam } from "~/context/TeamProvider";
 import { notFound } from "next/navigation";
 import TeamMembersTable from "~/app/dashboard/teams/[id]/(owner)/members/members-table";
+import TeamInvitesTable from "~/app/dashboard/teams/[id]/(owner)/members/invites-table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 
 export default function TeamMembersPage() {
   const team = useTeam()
+  const [tab, setTab] = React.useState<string>("members");
   if(!(team)) {
     return notFound()
   }
@@ -16,6 +19,21 @@ export default function TeamMembersPage() {
   }
 
   return (
-    <TeamMembersTable />
+    <Tabs defaultValue={tab} className="w-full">
+      <TabsList className="w-full">
+        <TabsTrigger value="members" onClick={() => setTab("members")}>
+          Mitglieder
+        </TabsTrigger>
+        <TabsTrigger value="invites" onClick={() => setTab("invites")}>
+          Einladungen
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="members">
+        <TeamMembersTable />
+      </TabsContent>
+      <TabsContent value="invites">
+        <TeamInvitesTable />
+      </TabsContent>
+    </Tabs>
   );
 }
