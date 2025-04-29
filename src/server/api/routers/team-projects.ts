e1568fa393a212c00269388165beb896a13db437
jan <scherping.jan@icloud.com>
2025-04-29 19:43:06 +0200

    add createTask route

--- src/server/api/routers/team-projects.ts
+++ src/server/api/routers/team-projects.ts
@@ -11,24 +11,53 @@ export const teamProjectsRouter = createTRPCRouter({
     }),
 
   find: publicProcedure
-    .input(z.object({id: z.number()}))
+    .input(z.object({ id: z.number() }))
     .query(async ({ ctx, input }) => {
-      return ctx.db.project.findUnique({ where: { id: Number(input.id) },
-      include: {
-        ProjectTask: true
-      }})
+      return ctx.db.project.findUnique({
+        where: { id: Number(input.id) },
+        include: {
+          ProjectTask: true,
+        },
+      });
     }),
 
   create: publicProcedure
-    .input(z.object({name: z.string(), description: z.string().optional(), userId: z.string(), teamId: z.number()}))
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string().optional(),
+        userId: z.string(),
+        teamId: z.number(),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       return ctx.db.project.create({
         data: {
           name: input.name,
           description: input.description,
           createdById: Number(input.userId),
-          teamId: input.teamId
-        }
-      })
-    })
+          teamId: input.teamId,
+        },
+      });
+    }),
+
+  createTask: publicProcedure
+    .input(
+      z.object({
+        task: z.string(),
+        description: z.string(),
+        userId: z.string(),
+        projectId: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.projectTask.create({
+        data: {
+          task: input.task,
+          description: input.description,
+          createdById: Number(input.userId),
+          projectId: input.projectId,
+        },
+      });
+    }),
 });
