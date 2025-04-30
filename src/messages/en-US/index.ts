7d33ba6be191b082813855dee05088c8533c719e
jan <scherping.jan@icloud.com>
2025-05-01 00:22:01 +0200

    add english translations (source: ChatGPT)

--- src/messages/en-US/index.ts
+++ src/messages/en-US/index.ts
@@ -1,7 +1,13 @@
-import loginLocale from "./login.json"
-import errorsLocale from "./errors.json"
+import commonLocale from "./common.json";
+import userLocale from "./user.json";
+import teamLocale from "./team.json";
+import loginLocale from "./login.json";
+import errorsLocale from "./errors.json";
 
 export default {
+  common: commonLocale,
+  user: userLocale,
+  team: teamLocale,
   login: loginLocale,
   errors: errorsLocale,
-}
+};
