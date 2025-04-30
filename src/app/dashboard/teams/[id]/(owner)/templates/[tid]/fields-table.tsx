d9c7831aee7ca45ad614d956c36e795c6f5f3715
jan <scherping.jan@icloud.com>
2025-05-01 00:05:49 +0200

    translate pages

--- src/app/dashboard/teams/[id]/(owner)/templates/[tid]/fields-table.tsx
+++ src/app/dashboard/teams/[id]/(owner)/templates/[tid]/fields-table.tsx
@@ -21,7 +21,7 @@ export default function TemplateFieldsTable({
   templateId: number;
   fields: TemplateField[];
 }) {
-  const t = useTranslations()
+  const t = useTranslations();
   const [createOpen, setCreateOpen] = React.useState<boolean>(false);
   const [deleteId, setDeleteId] = React.useState<number | null>(null);
   const [tableData, setTableData] = React.useState<TemplateField[]>([]);
@@ -29,13 +29,19 @@ export default function TemplateFieldsTable({
   const deleteMutation = api.templateField.delete.useMutation();
   const updateOrderMutation = api.templateField.updateOrder.useMutation({
     onMutate: () => {
-      showToast("Lädt", "Die Reihenfolge wird aktualisiert...");
+      showToast(
+        t("common.table.save-order.loading.title"),
+        t("common.table.save-order.loading.message"),
+      );
     },
     onSuccess: () => {
-      showToast("Erledigt", "Die Reihenfolge wurde aktualisiert.");
+      showToast(
+        t("common.table.save-order.success.title"),
+        t("common.table.save-order.success.message"),
+      );
     },
     onError: () => {
-      showErrorToast();
+      showErrorToast(t);
     },
   });
 
@@ -44,10 +50,10 @@ export default function TemplateFieldsTable({
   }, [fields]);
 
   const columns: ColumnDef<TemplateField>[] = [
-    centeredColumn("label", "Bezeichnung"),
-    centeredColumn("placeHolder", "Platzhalter"),
-    centeredColumn("fieldType", "Typ"),
-    TableActions(t("common.table.actions"),null, (id) => setDeleteId(id)),
+    centeredColumn("label", t("team.field.label")),
+    centeredColumn("placeHolder", t("team.field.placeHolder")),
+    centeredColumn("fieldType", "common.type"),
+    TableActions(t("common.table.actions"), null, (id) => setDeleteId(id)),
   ];
 
   return (
