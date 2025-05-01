060b0e8dbdc9d70a9aed623bbb6ed1478c0a5b4b
jan <scherping.jan@icloud.com>
2025-05-01 20:33:08 +0200

    add disabled state

--- src/components/instance/signature-pad.tsx
+++ src/components/instance/signature-pad.tsx
@@ -10,9 +10,15 @@ type PageProps = {
   defaultValue?: string | null;
   action?: (value: string | undefined) => void;
   t: TranslationFunction;
+  disabled: boolean;
 };
 
-export default function SignaturePad({ defaultValue, action, t }: PageProps) {
+export default function SignaturePad({
+  defaultValue,
+  action,
+  t,
+  disabled,
+}: PageProps) {
   const theme = useTheme();
   const sigCanvasRef = useRef<SignatureCanvas>(null);
   const penColor = theme.systemTheme == "dark" ? "white" : "black";
@@ -23,6 +29,13 @@ export default function SignaturePad({ defaultValue, action, t }: PageProps) {
       sigCanvasRef.current.fromDataURL(defaultValue);
     }
   }, [defaultValue]);
+
+  useEffect(() => {
+    if (!disabled) return;
+    if (!sigCanvasRef.current) return;
+    const canvas = sigCanvasRef.current.getCanvas();
+    canvas.style.pointerEvents = "none";
+  }, [disabled]);
 
   const clear = () => {
     sigCanvasRef.current?.clear();
@@ -46,14 +59,16 @@ export default function SignaturePad({ defaultValue, action, t }: PageProps) {
           className: "w-full border rounded-md",
         }}
       />
-      <div className="mt-2 ml-auto space-x-2">
-        <Button onClick={clear} variant={"destructive"}>
-          {t("signature.clear")}
-        </Button>
-        <Button onClick={save} variant={"default"}>
-          {t("common.save")}
-        </Button>
-      </div>
+      {!disabled && (
+        <div className="mt-2 ml-auto space-x-2">
+          <Button onClick={clear} variant={"destructive"}>
+            {t("signature.clear")}
+          </Button>
+          <Button onClick={save} variant={"default"}>
+            {t("common.save")}
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
