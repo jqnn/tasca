118c89ec4db6f84e59b51ae24e1d2871d0a53192
jan <scherping.jan@icloud.com>
2025-05-01 10:24:02 +0200

    add title to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ export default async function LoginPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <h1 className={"p-4 text-center text-xl font-bold"}>tasca</h1>
+
         <LoginForm />
       </div>
     </div>
