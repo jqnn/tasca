dcc22bf003e31888d17cfcc0f1fe6865ea7156bf
jan <scherping.jan@icloud.com>
2025-05-06 15:54:43 +0200

    fix redirect bug

--- src/app/dashboard/invites/invite-list.tsx
+++ src/app/dashboard/invites/invite-list.tsx
@@ -34,7 +34,7 @@ export default function TeamInvitesTable() {
         t("user.invite.accepted.title"),
         t("user.invite.accepted.message"),
       );
-      router.push(`/dashboard/teams/${data.id}`);
+      router.push(`/dashboard/teams/${data.teamId}`);
     },
     onError: () => {
       showErrorToast(t);
