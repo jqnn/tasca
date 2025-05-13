bf36dc621b68bd9bcbdb45dc1b6aed4e71453e4e
jan <scherping.jan@icloud.com>
2025-05-13 18:47:58 +0200

    add debounce, so value automatically update after 350ms no typing

--- src/components/instance/field-input.tsx
+++ src/components/instance/field-input.tsx
@@ -7,6 +7,8 @@ import type { InstanceField, TemplateField } from "@prisma/client";
 import { api } from "~/trpc/react";
 import { showErrorToast } from "~/lib/utils";
 import { useTranslations } from "next-intl";
+import debounce from "lodash.debounce";
+import type { DebouncedFunc } from "lodash";
 
 export default function FieldInput({
   instance,
@@ -18,39 +20,59 @@ export default function FieldInput({
   disabled: boolean;
 }) {
   const t = useTranslations();
-
-  const handleBlur = () => {
-    updateMutation.mutate({
-      id: instance.id,
-      value: value,
-    });
-  };
+  const [value, setValue] = React.useState<string>(instance.value);
 
   const updateMutation = api.instance.updateValue.useMutation({
-    onError: () => {
-      showErrorToast(t);
-    },
+    onError: () => showErrorToast(t),
     onSuccess: () => {
       instance.value = value;
     },
   });
 
-  const [value, setValue] = React.useState<string>(instance.value);
+  const updateMutationRef = React.useRef(updateMutation);
+  const instanceIdRef = React.useRef(instance.id);
+
+  React.useEffect(() => {
+    updateMutationRef.current = updateMutation;
+    instanceIdRef.current = instance.id;
+  }, [updateMutation, instance.id]);
+
+  const debouncedUpdate = React.useMemo<
+    DebouncedFunc<(value: string) => void>
+  >(() => {
+    return debounce((newValue: string) => {
+      updateMutationRef.current.mutate({
+        id: instanceIdRef.current,
+        value: newValue,
+      });
+    }, 350);
+  }, []);
+
+  React.useEffect(() => {
+    return () => {
+      debouncedUpdate.cancel();
+    };
+  }, [debouncedUpdate]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    debouncedUpdate(newValue);
+  };
 
   return (
-    <div className="grid w-full grid-cols-4">
-      <Label className={"mr-auto ml-auto"} htmlFor={String(instance.id)}>
+    <div className="grid w-full grid-cols-4 items-center gap-2">
+      <Label className="mr-auto ml-auto" htmlFor={String(instance.id)}>
         {field.label}
       </Label>
       <Input
         id={String(instance.id)}
         value={value}
-        className={"col-span-3"}
+        className="col-span-3"
         placeholder={field.placeHolder ?? field.label}
         type={field.fieldType}
         disabled={disabled}
-        onChange={(e) => setValue(e.target.value)}
-        onBlur={handleBlur}
+        onChange={handleChange}
       />
     </div>
   );
